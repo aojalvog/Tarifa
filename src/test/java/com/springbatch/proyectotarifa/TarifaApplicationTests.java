5a8a65e171f7bfa8327dc2b0e833b2079f5db926
aojalvog <alvaro.ojalvo@gmail.com>
2024-05-08 15:18:17 +0200

    feat(Creación del proyecto): :sparkles:

---
+++ src/test/java/com/springbatch/proyectotarifa/TarifaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatch.proyectotarifa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarifaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
