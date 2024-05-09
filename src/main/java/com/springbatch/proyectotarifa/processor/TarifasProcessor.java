9340e8b14ac5fd462efd98f579d613f984ca9e62
aojalvog <alvaro.ojalvo@gmail.com>
2024-05-09 12:44:06 +0200

    docs(Documentation): :memo:

--- src/main/java/com/springbatch/proyectotarifa/processor/TarifasProcessor.java
+++ src/main/java/com/springbatch/proyectotarifa/processor/TarifasProcessor.java
@@ -4,7 +4,23 @@ import org.springframework.batch.item.ItemProcessor;
 
 import com.springbatch.proyectotarifa.model.Tarifas;
 
+/**
+ * 
+ * Esta clase implementa la interfaz ItemProcessor de Spring Batch para procesar
+ * objetos de tipo {@link Tarifas} En este caso, el procesador simplemente
+ * devuelve la misma tarifa sin aplicar ninguna transformación.
+ */
+
 public class TarifasProcessor implements ItemProcessor<Tarifas, Tarifas> {
+	/**
+	 * Método que procesa un objeto {@link Tarifas}. En este caso, el procesador
+	 * simplemente devuelve la misma tarifa sin aplicar ninguna transformación.
+	 * 
+	 * @param tarifa La tarifa a procesar.
+	 * @return La misma tarifa sin cambios.
+	 * @throws Exception Si ocurre algún error durante el procesamiento.
+	 */
+
 	@Override
 	public Tarifas process(Tarifas tarifa) throws Exception {
 		return tarifa;
--- src/main/java/com/springbatch/proyectotarifa/writer/Writer.java
+++ src/main/java/com/springbatch/proyectotarifa/writer/Writer.java
@@ -10,9 +10,25 @@ import org.springframework.stereotype.Component;
 
 import com.springbatch.proyectotarifa.model.Tarifas;
 
+/**
+ * 
+ * Esta clase representa un componente de Spring que proporciona funcionalidad
+ * para escribir objetos de tipo Tarifas en un archivo plano. Utiliza un
+ * FlatFileItemWriter para escribir los objetos Tarifas en un archivo CSV.
+ */
 @Component
 
 public class Writer {
+	/**
+	 * Método que configura y devuelve un FlatFileItemWriter para escribir objetos
+	 * Tarifas en un archivo plano CSV. Utiliza un extractor de campos
+	 * BeanWrapperFieldExtractor para extraer los campos de los objetos Tarifas.
+	 * Utiliza un agregador de líneas DelimitedLineAggregator para agregar los
+	 * campos en una línea del archivo plano.
+	 * 
+	 * @return Un FlatFileItemWriter configurado para escribir objetos Tarifas en un
+	 *         archivo plano CSV.
+	 */
 
 	@Bean
 	FlatFileItemWriter<Tarifas> itemWriter() {
